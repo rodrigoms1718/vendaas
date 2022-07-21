fdb00819041e869785a97a3275c139dea31eed1b
rodrigoms1718 <rodrigoms1718@gmail.com>
2022-07-21 00:56:23 -0300

    Concluindo o cadastro de clientes com JDBC

--- src/main/java/io/github/rodrigo/VendasApplication.java
+++ src/main/java/io/github/rodrigo/VendasApplication.java
@@ -30,11 +30,38 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes){
         return  args -> {
+            System.out.println("Salvando Clientes");
+
             clientes.salvar(new Cliente("Rodrigo"));
             clientes.salvar(new Cliente("Outro Cliente"));
 
             List<Cliente> todosCLientes = clientes.obterTodos();
             todosCLientes.forEach(System.out::println);
+
+
+            System.out.println("Atualizando Clientes");
+            todosCLientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosCLientes = clientes.obterTodos();
+            todosCLientes.forEach(System.out::println);
+
+            System.out.println("Buscando Clientes");
+            clientes.buscarPorNome("Cli").forEach(System.out::println);
+
+            System.out.println("Deletando Clientes");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
+
+            todosCLientes = clientes.obterTodos();
+            if(todosCLientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado.");
+            }else{
+                todosCLientes.forEach(System.out::println);
+            }
         };
     }
 
