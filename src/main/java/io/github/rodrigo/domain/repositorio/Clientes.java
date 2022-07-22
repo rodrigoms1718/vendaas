ce17de62c57617b74dcfd3f90fe374d232220b5f
rodrigoms1718 <rodrigoms1718@gmail.com>
2022-07-21 21:40:02 -0300

    Trabalhando com @Query nativo

--- src/main/java/io/github/rodrigo/domain/repositorio/Clientes.java
+++ src/main/java/io/github/rodrigo/domain/repositorio/Clientes.java
@@ -2,6 +2,7 @@ package io.github.rodrigo.domain.repositorio;
 
 import io.github.rodrigo.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,8 +10,12 @@ import java.util.List;
 
 public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-    @Query(value = "select c from Cliente c where c.nome like :nome")
+    @Query(value = "select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
     List<Cliente> encontrarPorNome(@Param("nome") String nome);
+
+    @Query(" delete from Cliente c where c.nome =:nome ")
+    @Modifying
+    void deleteByNome(String nome);
 
     boolean existsByNome(String nome);
 }
