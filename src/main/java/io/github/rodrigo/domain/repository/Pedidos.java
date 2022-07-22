9ce12e01c70504e018c11c82b935276ec2b305d4
rodrigoms1718 <rodrigoms1718@gmail.com>
2022-07-22 17:57:56 -0300

    Carregando os pedidos por cliente

--- src/main/java/io/github/rodrigo/domain/repository/Pedidos.java
+++ src/main/java/io/github/rodrigo/domain/repository/Pedidos.java
@@ -1,7 +1,11 @@
 package io.github.rodrigo.domain.repository;
 
+import io.github.rodrigo.domain.entity.Cliente;
 import io.github.rodrigo.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Pedidos extends JpaRepository<Pedido, Integer> {
+    List<Pedido> findByCliente(Cliente cliente);
 }
