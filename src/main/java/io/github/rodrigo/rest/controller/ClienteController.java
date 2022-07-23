5b81fe5979511433f86333082ec3a77c8bf7de90
rodrigoms1718 <rodrigoms1718@gmail.com>
2022-07-23 04:39:58 -0300

    Delete Mapping: deletando um recurso no servidor

--- src/main/java/io/github/rodrigo/rest/controller/ClienteController.java
+++ src/main/java/io/github/rodrigo/rest/controller/ClienteController.java
@@ -33,4 +33,17 @@ public class ClienteController {
         Cliente clienteSalvo = clientes.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
     }
+
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
