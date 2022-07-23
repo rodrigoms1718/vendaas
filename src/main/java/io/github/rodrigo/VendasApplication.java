c5354b626c5857c7fa8bbe8baace549c083bff7d
rodrigoms1718 <rodrigoms1718@gmail.com>
2022-07-23 03:43:25 -0300

    Criando o controller de clientes

--- src/main/java/io/github/rodrigo/VendasApplication.java
+++ src/main/java/io/github/rodrigo/VendasApplication.java
@@ -1,60 +1,10 @@
 package io.github.rodrigo;
 
-import io.github.rodrigo.domain.entity.Cliente;
-import io.github.rodrigo.domain.entity.Pedido;
-import io.github.rodrigo.domain.repository.Clientes;
-import io.github.rodrigo.domain.repository.Pedidos;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
-
-    @Value("${application.name}")
-    private String applicationName;
-
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return applicationName;
-    }
-
-    @Bean
-    public CommandLineRunner init(
-            @Autowired Clientes clientes,
-            @Autowired Pedidos pedidos
-            ){
-        return  args -> {
-            System.out.println("Salvando Clientes");
-
-            Cliente fulano = new Cliente("Fulano");
-            clientes.save(fulano);
-
-            Pedido p = new Pedido();
-            p.setCliente(fulano);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-//            Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
-//            System.out.println(cliente);
-//            System.out.println(cliente.getPedidos());
-
-            pedidos.findByCliente(fulano).forEach(System.out::println);
-
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
