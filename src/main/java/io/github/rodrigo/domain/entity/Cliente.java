021633d53fbb5ba5b3edc00d0990b0739ee6345a
rodrigoms1718 <rodrigoms1718@gmail.com>
2022-07-23 13:41:25 -0300

    Pesquisa de Clientes por parâmetros

--- src/main/java/io/github/rodrigo/domain/entity/Cliente.java
+++ src/main/java/io/github/rodrigo/domain/entity/Cliente.java
@@ -17,6 +17,9 @@ public class Cliente {
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @Column(name = "cpf", length = 11)
+    private String cpf;
+
     @JsonIgnore
     @OneToMany( mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos;
@@ -57,11 +60,20 @@ public class Cliente {
         this.pedidos = pedidos;
     }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
     @Override
     public String toString() {
         return "Cliente{" +
                 "id=" + id +
                 ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
                 '}';
     }
 }
--- src/main/java/io/github/rodrigo/rest/controller/ClienteController.java
+++ src/main/java/io/github/rodrigo/rest/controller/ClienteController.java
@@ -2,10 +2,13 @@ package io.github.rodrigo.rest.controller;
 
 import io.github.rodrigo.domain.entity.Cliente;
 import io.github.rodrigo.domain.repository.Clientes;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -58,4 +61,20 @@ public class ClienteController {
                     return ResponseEntity.noContent().build();
                 }).orElseGet( () -> ResponseEntity.notFound().build() );
     }
+
+    @GetMapping("/api/clientes")
+    public ResponseEntity find(Cliente filtro){
+        ExampleMatcher matcher = ExampleMatcher
+                                    .matching()
+                                    .withIgnoreCase()
+                                    .withStringMatcher(
+                                            ExampleMatcher.StringMatcher.CONTAINING
+                                    );
+
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista = clientes.findAll(example);
+        return ResponseEntity.ok(lista);
+    }
+
+
 }
