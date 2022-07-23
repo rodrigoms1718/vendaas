47323eb127f449d12c04848148f7dd772449334e
rodrigoms1718 <rodrigoms1718@gmail.com>
2022-07-23 04:13:17 -0300

    Requisição GET com parâmentros e Response Entity

--- src/main/java/io/github/rodrigo/rest/controller/ClienteController.java
+++ src/main/java/io/github/rodrigo/rest/controller/ClienteController.java
@@ -1,18 +1,29 @@
 package io.github.rodrigo.rest.controller;
 
+import io.github.rodrigo.domain.entity.Cliente;
+import io.github.rodrigo.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/api/clientes")
 public class ClienteController {
+    private Clientes clientes;
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente){
-        return String.format("Hello %s ", nomeCliente);
+    public ResponseEntity getClienteById(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
