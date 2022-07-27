210bdb85330ab8ddf5492f269402e13ec2880a98
rodrigoms1718 <rodrigoms1718@gmail.com>
2022-07-27 01:45:04 -0300

    Validando a entidade Pedido

--- src/main/java/io/github/rodrigo/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/rodrigo/rest/dto/PedidoDTO.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -20,12 +21,14 @@ import java.util.List;
  *     ]
  * }
  */
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class PedidoDTO {
-
+    @NotNull(message = "Informe o código do cliente")
     private Integer cliente;
+
+    @NotNull(message = "O campo Total do pedido é obrigatório.")
     private BigDecimal total;
     private List<ItemPedidoDTO> items;
 }
